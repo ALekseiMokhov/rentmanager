6b85397608e78876b9abccf8ac7bd9c346b2b1a0
Aleksei Mokhov <alexeimohov@rambler.ru>
2020-05-27 19:25:59 +0300

    test class added

--- Task-3/CarService/test/TestApp.java
+++ Task-3/CarService/test/TestApp.java
@@ -1,11 +1,12 @@
 import java.time.LocalDate;
 
 
-public class Domain {
+public class TestApp {
     public static void main(String[] args) {
         Admin admin = new Admin();
         admin.addMaster( "Vasiliy", Speciality.MECHANIC);
         System.out.println(admin.findByName( "Vasiliy" ));
+
         admin.addPlace();
         admin.addPlace();
         admin.addPlace();
@@ -13,17 +14,19 @@ public class Domain {
             if(admin.getPlaces()[i]!=null)
             System.out.println(admin.getPlaces()[i].getId());
         }
+
+
         admin.addOrder(admin.findByName( "Vasiliy" ),admin.findPlace(),LocalDate.of( 2020,5,27 )  );
-     
         System.out.println(admin.findOrderById( 0 ).toString());
+
         admin.shiftOrderDate( admin.findOrderById( 0 ),LocalDate.of( 2020,5 ,28) );
         System.out.println(admin.findOrderById( 0 ).toString());
-        /**/
+        
         Order [] orders = admin.getOrders();
         for (Order order : orders) {
            if(order!=null) System.out.println(order.getId());
         }
-        /**/
+
 
         System.out.println(admin.findOrderById( 0 ).isDone());
         admin.removeOrder( 0 );
