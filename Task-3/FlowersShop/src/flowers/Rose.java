71e00a06ccbaba5a161219dff12e818be2814f19
Aleksei Mokhov <alexeimohov@rambler.ru>
2020-05-28 09:41:11 +0300

    Extracted builder class from Domain, brackets after if added

--- Task-3/FlowersShop/src/flowers/Rose.java
+++ Task-3/FlowersShop/src/flowers/Rose.java
@@ -5,20 +5,20 @@ import exceptions.WrongColorException;
 public class Rose implements Flower {
     private double price = 7.2;
     private FlowerColor flowerColor;
-    private int id =0;
+    private int id = 0;
 
 
-
-    public Rose( FlowerColor flowerColor) throws WrongColorException {
+    public Rose(FlowerColor flowerColor) throws WrongColorException {
         this.flowerColor = flowerColor;
-        if(flowerColor==FlowerColor.BLUE)  throw new WrongColorException( "There is no blue roses in Nature!" ) ;
+        if (flowerColor == FlowerColor.BLUE) {
+            throw new WrongColorException( "There is no blue roses in Nature!" );
+        }
 
     }
 
     public void setPrice(double price) {
         this.price = price;
     }
-
 
 
     @Override
