b24a1dd4b9164370e0d6b65ce94ab6b8ca34b9ea
Aleksei Mokhov <alexeimohov@rambler.ru>
2020-07-29 18:35:31 +0300

    changed sleeping time in ex 3

--- Task-9/src/exercise_3/App.java
+++ Task-9/src/exercise_3/App.java
@@ -7,7 +7,7 @@ public class App {
 
     private static final Queue <Integer> BUFFER = new ArrayDeque <>();
     private static final Integer MAX_SIZE = 10;
-    
+
     public static void main(String[] args) {
         Thread thread1 = new Thread( new Producer() );
         Thread thread2 = new Thread( new Consumer() );
@@ -20,22 +20,17 @@ public class App {
         @Override
         public void run() {
             while (true) {
-                    while (BUFFER.size() == MAX_SIZE) {
-                        try {
-                            Thread.currentThread().sleep( getRandomLong());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
+                while (BUFFER.size() == MAX_SIZE) {
                     try {
-                        Thread.currentThread().sleep( getRandomLong() );
+                        Thread.currentThread().sleep( 1000 );
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    int x = getRandom();
-                    BUFFER.add( x );
-                    System.out.println( "Integer produced: " + x );
+                }
+                int x = getRandom();
+                BUFFER.add( x );
+                System.out.println( "Integer produced: " + x );
+
             }
         }
     }
@@ -45,20 +40,20 @@ public class App {
         public void run() {
             while (true) {
 
-                    while (BUFFER.size() == 0) {
-                        try {
-                            Thread.currentThread().sleep( getRandomLong() );
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
+                while (BUFFER.size() == 0) {
                     try {
-                        Thread.currentThread().sleep( getRandomLong());
+                        Thread.currentThread().sleep( 1000 );
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    System.out.println( "Integer consumed: " + BUFFER.poll() );
+                }
+
+                try {
+                    Thread.currentThread().sleep( getRandomLong() );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println( "Integer consumed: " + BUFFER.poll() );
 
             }
         }
