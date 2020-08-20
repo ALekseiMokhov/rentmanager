2ddefbfd661509e50fec839d57ed5666801857fa
Aleksei Mokhov <alexeimohov@rambler.ru>
2020-08-20 21:59:16 +0300

    _

--- Task-11/ddl.sql
+++ Task-11/ddl.sql
@@ -1,9 +1,13 @@
 DROP SCHEMA IF EXISTS senla;
+
 CREATE SCHEMA senla;
+
 USE senla;
+
 CREATE TABLE place (
 id UUID NOT NULL PRIMARY KEY,
 calendar ARRAY NOT NULL);
+
 CREATE TABLE master(
 id UUID NOT NULL PRIMARY KEY,
 calendar ARRAY NOT NULL,
@@ -11,4 +15,19 @@ fullname VARCHAR(255) NOT NULL,
 dailypayment DOUBLE NOT NULL,
 speciality ENUM('MECHANIC','RESHAPER','ELECTRICIAN',
 'PAINTER') NOT NULL);
-CREATE TABLE order
+
+CREATE TABLE orders(
+id UUID NOT NULL PRIMARY KEY,
+date_of_booking TIMESTAMP  NOT NULL,
+beginning_date TIMESTAMP,
+finish_date TIMESTAMP,
+orderstatus ENUM('CANCELLED','COMPLETED','MANAGED') NOT NULL,
+id_place NOT NULL,
+FOREIGN KEY(id_place) REFERENCES place(id));
+
+
+CREATE TABLE orders_masters(
+id_orders UUID NOT NULL ,
+id_masters UUID NOT NULL ,
+FOREIGN KEY(id_orders) REFERENCES orders(id),
+FOREIGN KEY(id_masters) REFERENCES master(id))
